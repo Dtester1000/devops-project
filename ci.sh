3510b94d76c2e15c6fdefebe91a0b7b137a8f15e
Dtester1000 <tttestyr@gmail.com>
2025-03-29 18:41:13 +0300

    test1

--- ci.sh
+++ ci.sh
@@ -1,33 +1,17 @@
 #!/bin/bash
 set -e
 
-USER_EMAIL='tttestyr@gmail.com'
-USER_NAME='Dtester1000'
-
-REPO_URL="https://github.com/Dtester1000/Toy-Store-DevOps.git"
 commit_message= read -p "Enter commit message: "
 
 cd Toy-Store
-
-if [ -z "$(git remote -v)" ]; then
-    git remote add origin "$REPO_URL"
-    echo "Remote url "$REPO_URL" added."
-else
-    echo "Remote url already set changing it....."
-    git remote set-url origin "$REPO_URL"
-fi
-
-git config --global user.email "$USER_EMAIL"
-git config --global user.name "$USER_NAME"
+docker login
 git pull origin main
-wait
 git add .
-wait
-git commit -m "my commit"
-wait
+git commit -m "$commit_message"
 REPO_URL="https://github.com/Dtester1000/devops-project.git"
 git remote set-url origin "$REPO_URL"
-wait
-git branch -M main
+
+docker build -t test3090/toy-store:0.0.1 .
+docker push test3090/toy-store:0.0.1
 git push origin main
-echo "Pushed to remote repository"
+echo "All files pushed to remote repositories"
