731f6e1a51bb580aa5e83d9813acfa4829f73abc
Dtester1000 <tttestyr@gmail.com>
2025-03-29 19:03:42 +0300

    github action

--- ci.sh
+++ ci.sh
@@ -11,7 +11,7 @@ git commit -m "$commit_message"
 REPO_URL="https://github.com/Dtester1000/devops-project.git"
 git remote set-url origin "$REPO_URL"
 
-docker build -t test3090/toy-store:0.0.1 .
-docker push test3090/toy-store:0.0.1
+#docker build -t test3090/toy-store:0.0.1 .
+#docker push test3090/toy-store:0.0.1
 git push origin main
 echo "All files pushed to remote repositories"
