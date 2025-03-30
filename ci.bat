772f2bbf2627f31c0b53630adc742b90a6bc4dfb
Dtester1000 <tttestyr@gmail.com>
2025-03-30 13:51:40 +0300

    last update

--- ci.bat
+++ ci.bat
@@ -1,12 +1,13 @@
 @echo off
 setlocal
 
-set /p commit_message = "Enter commit message: "    
+set /p commit_message="Enter commit message: "    
 
 cd Toy-Store
 docker login
 git pull origin main
 git add .
+REM git rm --cached Toy-Store -f
 git commit -m "%commit_message%"
 git push origin main
 echo "All files pushed to remote repositories"
